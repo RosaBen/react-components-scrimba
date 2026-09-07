c0bdc6937864fd6b683f8cfcbf5ac98c2d3da50e
RosaBen <benchabane.rosa@outlook.com>
2026-09-07 04:54:47 +0200

    css table done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // packages
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 // components
 // pages
 import Header from "./components/Header";
+
 // styles
-import "./assets/styles/responsive.css";
 import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 
 // scripts
 // images
